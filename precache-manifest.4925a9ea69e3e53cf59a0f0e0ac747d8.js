634c068770373f5f61d4db1d2bb714f0cb794cd8
tonypark <typ5120@gmail.com>
2021-06-17 22:39:40 -0400

    Updates

---
+++ precache-manifest.4925a9ea69e3e53cf59a0f0e0ac747d8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90ea27c7e711e4c840c7eb499212b5d",
+    "url": "/note_app/index.html"
+  },
+  {
+    "revision": "72e20e02db8c2a57e893",
+    "url": "/note_app/static/js/2.2a03aff8.chunk.js"
+  },
+  {
+    "revision": "5d6c3fea7d0db6d4f90b",
+    "url": "/note_app/static/js/main.a7356348.chunk.js"
+  },
+  {
+    "revision": "3d689ac19240b41fac5a",
+    "url": "/note_app/static/js/runtime-main.3771ed6f.js"
+  }
+]);
